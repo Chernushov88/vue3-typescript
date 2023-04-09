504896657db74d5e4f96ef3d7d0acc4ad4e3ce6b
chernushov88@gmail.com <chernushov88@gmail.com>
2023-04-09 22:08:32 +0100

    Vue Components using TypeScript

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
+  <router-view>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  </router-view>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Zelda')">change name</button>
+    <button @click="changeAge('30')">change age</button>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      name: 'Link',
+      age: 25 as number | string
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name
+      return name
+    },
+    changeAge(age: number | string) {
+      this.age = age
+      return age
+    }
+  }
+});
+</script>

0e4056d4479404921dd6201adc4d8ef4fcc3cb8e
chernushov88@gmail.com <chernushov88@gmail.com>
2023-04-10 14:59:19 +0100

    Types & Props

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,24 @@
 <template>
-  <router-view>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  </router-view>
+  
   <div class="app">
-<!--    <p>{{ name }} - {{ age }}</p>-->
-<!--    <button @click="changeName('Zelda')">change name</button>-->
-<!--    <button @click="changeAge('50')">change age</button>-->
-    <table border-collapse="collapse" border="1" cellpadding="3">
-      <tbody>
-      <tr v-for="item in jobs" :key="item.title">
-        <td v-for="elem in item" :key="elem"> {{elem}} </td>
-      </tr>
-      </tbody>
-    </table>
+		<router-view>
+			<nav>
+				<router-link to="/">Home</router-link> |
+				<router-link to="/about">About</router-link>
+			</nav>
+		</router-view>
+		<JobsListVue :jobs="jobs"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref, toRefs } from 'vue';
 import Job from '@/types/Job'
+import JobsListVue from './components/JobsList.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {},
+  components: {JobsListVue},
   setup() {
     /*const state = reactive({
       name: 'Link',
---
+++ src/components/JobsList.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="lob-list">
+		<ul>
+			<li v-for="job in jobs" :key="job.id">
+				<h2>{{ job.title }} in {{ job.location }}</h2>
+				<div class="salary">
+					<p>{{ job.salary }} rupees</p>
+				</div>
+				<div class="description">
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum reiciendis ad, neque, odio dignissimos reprehenderit earum ab quod nesciunt amet consequuntur? Quae harum laborum delectus molestias neque, officiis magni sint.</p>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import Job from '@/types/Job'
+
+export default defineComponent({
+	props: {
+		jobs: {
+			required: true,
+			type: Array as PropType<Job[]>
+		}
+	}
+})
+</script>
+
+<style scoped>
+
+</style>
